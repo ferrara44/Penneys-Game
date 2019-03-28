a76a5eb5c1b77cfd773c65b8755ee229cd89eeaa
Facundo Ferrara <ferraraphys@gmail.com>
2019-03-28 11:18:31 -0300

    Non working implementation
    This is a proyect from 2018, the first of a couple broken programs I'll try to get working in my free time.

---
+++ penneys.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char predic1[5];
+char predic2[5];
+int sigue=1;
+
+int secuencia[256];
+int i,j;
+
+void jug1(){
+printf("Player 1's Guess: ");
+fgets(predic1, sizeof(3), stdin);
+getchar();
+}
+
+void jug2(){
+printf("Player 2's Guess: ");
+fgets(predic2, sizeof(3), stdin);
+getchar();
+}
+
+int evaluar(int a){
+	if (a==0 && secuencia[i-a]==predic1[3-a] ) {
+		printf("1");
+		return 1;
+	}
+	else if (secuencia[i-a]==predic1[3-a]) evaluar(a-1);
+	else return 0;
+}
+
+int evaluar2(int a){
+	if (a==0 && secuencia[i-a]==predic2[3-a] ) {
+		printf("2");
+		return 1;
+	}
+	else if (secuencia[i-a]==predic2[3-a]) evaluar(a-1);
+	else return 0;
+}
+
+int main(int argc, char *argv[]) {
+	
+	srand(time(NULL));
+	
+	jug1();
+	
+	jug2();
+	
+	while (sigue==1){
+		int coc=(rand()%2);
+		if (coc==0) secuencia[i]='o';
+		else if (coc==1) secuencia[i]='x';
+		
+		printf("%c", secuencia[i]);
+		
+		if(evaluar(5)==1) sigue=0;
+		if(evaluar2(5)==1) sigue=0;
+		i++;
+	}
+	
+	
+	return 0;
+}
